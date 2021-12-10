5866c13614e572b0eaf330590b1c40ec1372d947
h-connor <62314329+h-connor@users.noreply.github.com>
2021-12-10 14:26:52 -0600

    updated to backtesting

--- RandomForest.py
+++ RandomForest.py
@@ -1,32 +1,38 @@
 import pandas as pd
-import numpy as np
+from sklearn import metrics
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn import metrics
+import statistics
 
 # Creating a DataFrame of dataset
 data = pd.read_csv('featureExtractionFinal.csv', low_memory=False)
-data.head()
+X = data[['AmountSent', 'Income', 'Neighbours', 'CoAddresses', 'Successors']]  # Features
+y = data['IsRansome']  # Labels
 
-data.shape
+# Now, we simulate 'time' by training and testing on the next address incrementally
 
-X=data[['Year', 'Day', 'AmountSent', 'Income', 'Neighbours', 'CoAddresses', 'Successors']]  # Features
-y=data['IsRansome']  # Labels
+accuracy = []
+print_every_x = 5000
 
-# Split dataset into training set and test set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+i = 0
+while i < len(data):
+    i += 2
+    subSetX = X[:i]
+    subSetY = y[:i]
 
-#Create a Classifier
-clf=RandomForestClassifier(n_estimators=100)
+    # Split dataset into training set and test set
+    X_train, X_test, y_train, y_test = train_test_split(subSetX, subSetY, test_size=0.5)
 
-df.dropna(inplace=True)
+    # Create a Classifier
+    clf = RandomForestClassifier(n_estimators=50)
 
-#Train the model using the training sets 
-clf.fit(X_train,y_train)
-y_pred=clf.predict(X_test)
+    # Train the model using the training sets
+    clf.fit(X_train, y_train)
 
-print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+    y_pred = clf.predict(X_test)
+    accuracy.append(metrics.accuracy_score(y_test, y_pred))
 
+    if i % print_every_x == 0:
+        print('reached i: ' + str(i))
 
-#clf.predict([[2015, 25, 5.714149e+06,1.570000e+07,2,0,1]])
-
+print(statistics.mean(accuracy))
